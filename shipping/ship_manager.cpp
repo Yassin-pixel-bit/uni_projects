c76d80c38af24c1120f2c18a313672118593b866
Yassin-pixel-bit <yasyous2021@gmail.com>
2025-11-25 16:28:09 +0200

    added functionality to display ships and manage container addition

--- shipping/ship_manager.cpp
+++ shipping/ship_manager.cpp
@@ -4,6 +4,9 @@ void display_ships(Ship ships[], const int &count)
 {
     clear_terminal();
 
+    if (count == 0)
+        write_incolor("There are no ships to display\n", ERROR);
+
     for (int i = 0; i < count; i++)
     {
         Ship current_ship = ships[i];
@@ -13,8 +16,60 @@ void display_ships(Ship ships[], const int &count)
     }
 }
 
+int get_remaning_capacity(const Ship &ship)
+{
+    return ship.capacity - ship.used_capcity;
+}
+
+void add_containers(Ship &ship)
+{
+    // TODO: make sure that the containers weight don't exceed the ship's load capacity
+
+    int container_count;
+
+    cout << "How many containers do you want to add: ";
+    while(!(cin >> container_count) || container_count > MAX_CONTAINERS || container_count < 0)
+    {
+        clear_faulty_input("Please enter a number between [0 - 10]\n"); 
+    }
+
+    int space_left = MAX_CONTAINERS - ship.container_count;
+    if (container_count > space_left)
+    {
+        cout << "Only " << space_left << " container slots available. Adding maximum allowed.\n";
+        container_count = space_left;
+    }
+
+    if (container_count == 0)
+        return;
+
+    for (int i = 0; i < container_count; i++)
+    {
+        int weight;
+
+        cout << "how much should container " << i + 1 << " weight(in tons): ";
+        int remaning_weight = get_remaning_capacity(ship);
+
+        while(!(cin >> weight) || weight < 0 || weight > remaning_weight)
+        { 
+            clear_faulty_input("Please enter a positive number.\n");
+            cout << "enter a number less Than or equal to" << remaning_weight << endl;
+        }
+
+        ship.container[ship.container_count].weight = weight;
+        ship.used_capcity += weight;
+        ship.container_count++;
+    }
+}
+
 void add_ship(Ship ship[], int &ship_count)
 {
+    if (ship_count >= MAX_SHIPS)
+    {
+        write_incolor("Maximum ship limit reached.\n", INFO);
+        return;
+    }
+
     // TODO: read ship parameters from input and pass them to Ship constructor
     Ship new_ship;
 
@@ -32,6 +87,19 @@ void add_ship(Ship ship[], int &ship_count)
 
     cout << "departure day: ";
     new_ship.departure_day = read_int_in_range(1, 30);
+
+    char cont_agree;
+    do {
+        cout << "Do you want to add containers to this ship [y/n]: ";
+        cin >> cont_agree;
+        cont_agree = tolower(cont_agree);
+    } while (cont_agree != 'y' && cont_agree != 'n');
+
+    if (cont_agree == 'y')
+    {
+        clear_terminal();
+        add_containers(new_ship);
+    }
 
     // add the newly created ship to the ship array
     ship[ship_count] = new_ship;
--- shipping/ships.h
+++ shipping/ships.h
@@ -13,8 +13,9 @@ struct Ship {
     int number;
     std::string name;
     int capacity;
+    int used_capcity = 0;
     Container container[MAX_CONTAINERS];
-    int container_count;
+    int container_count = 0;
     int departure_day;
 };
 
