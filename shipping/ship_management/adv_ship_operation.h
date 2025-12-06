8ba166cc2ae156a5c22261cdb7ade3a890bd8fba
Yassin-pixel-bit <yasyous2021@gmail.com>
2025-12-06 18:56:56 +0200

    MINOR UPDATE

---
+++ shipping/ship_management/adv_ship_operation.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include "../utils.h"
+#include "../ships.h"
+
+
+using namespace std;
--- shipping/ship_managment/ship_operation.h
+++
@@ -1,3 +0,0 @@
-// Wrapper header: original path moved to `ship_management`.
-#pragma once
-#include "../ship_management/ship_operation.h"
